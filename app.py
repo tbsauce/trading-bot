370dc358acccab2d62b46d1008b4088cdc5bbf16
Sauce <telmobelasauce@ua.pt>
2024-01-15 03:17:31 +0000

    new selling strategy

--- app.py
+++ app.py
@@ -6,17 +6,8 @@ bot = Bot()
 symbol = "TSLA"
 time_frame = "5Min"
 #bearsih market
-# start_date = "2022-05-11T09:00:00Z"
-# end_date = "2022-05-12T11:30:00Z"
-#Bullish market
-# start_date = "2022-10-12T09:00:00Z"
-# end_date = "2022-10-12T11:30:00Z"
-
-# start_date = "2022-04-03T09:00:00Z"
-# end_date = "2022-05-04T11:30:00Z"
-
-start_date = "2021-01-03T09:00:00Z"
-end_date = "2022-11-04T11:30:00Z"
+start_date = "2022-01-01T09:00:00Z"
+end_date = "2023-11-30T11:30:00Z"
 
 feed = "sip"
 
@@ -31,8 +22,8 @@ data_volume = bot.get_volume(data)
 # Volume MA
 data_vma = bot.get_volume_moving_average(30, symbol, time_frame, start_date, end_date, feed)
 
-# Williams %R
-data_williams_r = bot.get_williams_r(30, symbol, time_frame, start_date, end_date, feed)
+# Williams %R 200 erro
+data_williams_r = bot.get_williams_r(200, symbol, time_frame, start_date, end_date, feed)
 
 # Strategy
 signals = []
@@ -48,31 +39,31 @@ for i in range(len(data['c'])):
     upper = data_donchian_channels['upper_band'].iloc[i]
     middle = data_donchian_channels['middle_band'].iloc[i]
 
-    if not buy and (sell_up <= closing or closing <= sell_down):
-        signals.append(-1 * closing)
-        buy = 1
-    elif (buy and upper <= closing and 
+    
+    good_to_buy = (upper <= closing and 
         data_volume['volume_bars'].iloc[i] > 0 and data_volume['volume_bars'].iloc[i -1] > 0 and
         data_vma['volume_ma'].iloc[i] < data_volume['volume_bars'].iloc[i] and
         data_vma['volume_ma'].iloc[i-1] < data_volume['volume_bars'].iloc[i-1] and
         data_volume['volume_bars'].iloc[i] > data_volume['volume_bars'].iloc[i-1] and 
-        data_williams_r['WilliamsR'].iloc[i] >= -20):
-        buy_value = closing
-        sell_down = closing - (closing * 0.001)
-        sell_up = closing + (closing * 0.002)
+        data_williams_r['WilliamsR'].iloc[i] >= -20)
+
+
+    if buy and good_to_buy:
+        sell_up = closing + (closing * 0.1)
+        sell_down = middle
         signals.append(closing)
         buy = 0
-    else:
+    elif not buy and (sell_up <= closing or closing <= sell_down):
+        signals.append(-1 * closing)
+        buy = 1
+    else: 
         signals.append(0)
-
-    tmp = closing - (closing * 0.001)
 
     if sell_down < middle:
         sell_down = middle
-    if not buy and sell_down < tmp:
-        sell_down = tmp
-
         
+
+    
     sell_down_values.append(sell_down)
     sell_up_values.append(sell_up)
 
